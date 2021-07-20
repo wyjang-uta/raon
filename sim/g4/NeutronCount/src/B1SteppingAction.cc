c916354dcea5b5579cd7fa71c0ea0812c75a8581
Wooyoung Jang <wyjang@thinkpadp53.thinkpadp53>
2021-07-20 17:00:26 -0500

    a pack of variables are declared to store the kinematic variables. some comments are also added about them.

--- sim/g4/NeutronCount/src/B1SteppingAction.cc
+++ sim/g4/NeutronCount/src/B1SteppingAction.cc
@@ -59,12 +59,24 @@ void B1SteppingAction::UserSteppingAction(const G4Step* step)
 
   G4int evt_type = 0;
 
+  // This routine records energy, momentum, and hit position of neutrons that are coming into the vacuum chamber.
+  // In order to identify neutrons coming into the chamber, we compare the step volume of the post-step point and the pre-step point.
+  // If the post-step point is "sensor_volume" but the pre-step point is some other volume, we accept those neutrons.
+  //
+  // To do: is there double counting issue?
+  // I think we can prevent this by rejecting the case that the pre-step volume and the post-step volume both are the 'sensor_volume'.
+  //
   if( presp == nullptr ) return;
   if( postsp == nullptr ) return;
   if( step->GetTrack()->GetParticleDefinition()->GetPDGEncoding() != 2112 ) return; // only neutrons
   if( postsp->GetTouchableHandle()->GetVolume() == nullptr ) return;
   if( presp->GetTouchableHandle()->GetVolume() == nullptr ) return;
   if( postsp->GetTouchableHandle()->GetVolume()->GetLogicalVolume()->GetName() != "sensor_volume" ) return; // save only neutrons passing decay chamber
+  if(
+      postsp->GetTouchableHandle()->GetVolume()->GetLogicalVolume()->GetName() == "sensor_volume" &&
+      presp->GetTouchableHandle()->GetVolume()->GetLogicalVolume()->GetName() == "sensor_volume" )
+    return; // we reject this case to prevent double counting issue.
+
   if( presp->GetTouchableHandle()->GetVolume()->GetLogicalVolume()->GetName() == "Dump")
   {
     evt_type = 1;  // This is for the case that neutron comes from the dump.
@@ -74,41 +86,35 @@ void B1SteppingAction::UserSteppingAction(const G4Step* step)
     evt_type = 2; // This is for the case that neutrons are coming from the world volume.
   }
 
-  G4double KE=(G4double)postsp->GetKineticEnergy()/CLHEP::MeV;
-  G4double TE=(G4double)postsp->GetTotalEnergy()/CLHEP::MeV;
-  G4double vx=(G4double)postsp->GetPosition().getX()/CLHEP::m;
+  G4double KE=(G4double)postsp->GetKineticEnergy()/CLHEP::MeV;    // Kinetic energy
+  G4double TE=(G4double)postsp->GetTotalEnergy()/CLHEP::MeV;      // Total energy
+  G4double vx=(G4double)postsp->GetPosition().getX()/CLHEP::m;    // Position where the neutron was produced.
   G4double vy=(G4double)postsp->GetPosition().getY()/CLHEP::m;
   G4double vz=(G4double)postsp->GetPosition().getZ()/CLHEP::m;
-  G4double px=(G4double)postsp->GetMomentum().getX()/CLHEP::MeV;
+  G4double px=(G4double)postsp->GetMomentum().getX()/CLHEP::MeV;  // Momentum of the neutron.
   G4double py=(G4double)postsp->GetMomentum().getY()/CLHEP::MeV;
   G4double pz=(G4double)postsp->GetMomentum().getZ()/CLHEP::MeV;
-  G4double theta=acos(pz/sqrt(px*px+py*py+pz*pz));
-  G4cout << "Sensor volume captured a neutron event!" << G4endl;
-  G4cout << "K=" << KE << "/ E=" << TE <<
-	  "/ vx=" << vx <<
-	  "/ vy=" << vy <<
-	  "/ vz=" << vz <<
-	  "/ px=" << px <<
-	  "/ py=" << py <<
-	  "/ pz=" << pz <<
-	  "/ theta=" << theta << G4endl;
+  G4double gt=(G4double)postsp->GetGlobalTime()/CLHEP::ns;
+  G4double lt=(G4double)postsp->GetLocalTime()/CLHEP::ns;
+  //G4double theta=acos(pz/sqrt(px*px+py*py+pz*pz));                // Direction of the neutron with respect to the z-axis (beam direction).
+
   // event type
   analysisManager->FillNtupleDColumn(0, (G4double)evt_type );
   // kineticEnergy
-  analysisManager->FillNtupleDColumn(1, (G4double)postsp->GetKineticEnergy()/CLHEP::MeV);
+  analysisManager->FillNtupleDColumn(1, KE);
   // total energy
-  analysisManager->FillNtupleDColumn(2, (G4double)postsp->GetTotalEnergy()/CLHEP::MeV);
+  analysisManager->FillNtupleDColumn(2, TE);
   // vertex coordinates
-  analysisManager->FillNtupleDColumn(3, (G4double)postsp->GetPosition().getX()/CLHEP::m);
-  analysisManager->FillNtupleDColumn(4, (G4double)postsp->GetPosition().getY()/CLHEP::m);
-  analysisManager->FillNtupleDColumn(5, (G4double)postsp->GetPosition().getZ()/CLHEP::m);
+  analysisManager->FillNtupleDColumn(3, vx);
+  analysisManager->FillNtupleDColumn(4, vy);
+  analysisManager->FillNtupleDColumn(5, vz);
   // momentum
-  analysisManager->FillNtupleDColumn(6, (G4double)postsp->GetMomentum().getX()/CLHEP::MeV);
-  analysisManager->FillNtupleDColumn(7, (G4double)postsp->GetMomentum().getY()/CLHEP::MeV);
-  analysisManager->FillNtupleDColumn(8, (G4double)postsp->GetMomentum().getZ()/CLHEP::MeV);
+  analysisManager->FillNtupleDColumn(6, px);
+  analysisManager->FillNtupleDColumn(7, py);
+  analysisManager->FillNtupleDColumn(8, pz);
   // time info
-  analysisManager->FillNtupleDColumn(9, (G4double)postsp->GetGlobalTime()/CLHEP::ns);
-  analysisManager->FillNtupleDColumn(10, (G4double)postsp->GetLocalTime()/CLHEP::ns);
+  analysisManager->FillNtupleDColumn(9, gt);
+  analysisManager->FillNtupleDColumn(10, lt);
   analysisManager->AddNtupleRow();
 }
 
