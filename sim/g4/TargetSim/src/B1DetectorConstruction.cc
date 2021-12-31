3b0cec26cc7eb99c1df238db8d1389499aa886cd
Wooyoung Jang <wyjang@master.tier3-atlas.uta.edu>
2021-12-30 20:07:12 -0600

    A cup-shaped neutron moderator is implemented.

--- sim/g4/TargetSim/src/B1DetectorConstruction.cc
+++ sim/g4/TargetSim/src/B1DetectorConstruction.cc
@@ -59,7 +59,6 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
   // Get nist material manager
   G4NistManager* nist = G4NistManager::Instance();
 
-
   // Option to switch on/off checking of volumes overlaps
   //
   G4bool checkOverlaps = true;
@@ -91,7 +90,7 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
                       checkOverlaps);        //overlaps checking
 
   //
-  // Fe dump
+  // W dump
   //
   G4double r_dump = 0.5 * m;
   G4double l_dump = 1.0 * m;
@@ -118,6 +117,107 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
                     false,                   //no boolean operation
                     0,                       //copy number
                     checkOverlaps);          //overlaps checking
+
+  //
+  // Neutron Moderator
+  //
+  G4double r_mod = 0.5 * m;
+  G4double l_mod = 0.2 * m; // thickness of the moderator is 20 cm
+  // Polyurethane C3H8N2O1
+  // Element definition from https://apc.u-paris.fr/~franco/g4doxy4.10/html/class_materials.html
+  // See the constructor source code line 54.
+  //
+  //                                          Z, A
+  G4Element* C = new G4Element("Carbon", "C", 6, 12.011*g/mole);
+  G4Element* H = new G4Element("Hydrogen", "H", 1, 1.00794*g/mole);
+  G4Element* N = new G4Element("Nitrogen", "N", 7, 14.00674*g/mole);
+  G4Element* O = new G4Element("Oxygen", "O", 8, 15.9994*g/mole);
+  G4Material* polyurethane = new G4Material(
+      "Polyurethane",   // its name
+      1100*kg/m3,       // its density
+      4,                // its number of elements
+      kStateSolid);     // its state
+  G4int natoms=3;
+  polyurethane->AddElement(C, natoms);
+  natoms=8;
+  polyurethane->AddElement(H, natoms);
+  natoms=2;
+  polyurethane->AddElement(N, natoms);
+  natoms=1;
+  polyurethane->AddElement(O, natoms);
+
+  G4Material* mod_mat = polyurethane;
+  //G4Material* mod_mat = nist->FindOrBuildMaterial("G4_B");
+
+  G4Tubs* solid_mod_volume =
+    new G4Tubs("mod_volume",
+        0.,           // inner radius
+        r_mod,        // outer raduis
+        0.5 * l_mod,  // half-length of the moderator
+        0.,           // starting phi
+        2.* M_PI * rad ); // ending phi (full cylinder)
+  G4LogicalVolume* logic_mod_volume =
+    new G4LogicalVolume(solid_mod_volume,
+        mod_mat,
+        "mod_volume");
+  new G4PVPlacement(
+      0,                                              // no rotation
+      G4ThreeVector(0, 0, 0.5 * ( l_dump + l_mod ) ), // translation
+      logic_mod_volume,                               // its logical volume
+      "mod_volume",                                   // its name
+      logicWorld,                                     // its mother volume
+      false,                                          // no boolean operation
+      0,                                              // a copy number
+      checkOverlaps);                                 // overlaps checking
+
+  // Neutron moderator sidewall
+  G4Tubs* solid_mod_sidewall =
+    new G4Tubs("mod_sidewall",
+        r_mod,
+        r_mod + l_mod,
+        0.5*(l_dump + l_mod),
+        0.,
+        2.* M_PI * rad );
+  G4LogicalVolume* logic_mod_sidewall =
+    new G4LogicalVolume(solid_mod_sidewall,
+        mod_mat,
+        "mod_sidewall");
+  new G4PVPlacement(
+      0,                              // no rotation
+      G4ThreeVector(0, 0, 0.5*l_mod), // translation
+      logic_mod_sidewall,             // its logical volume
+      "mod_sidewall",                 // its name
+      logicWorld,                     // its mother volume
+      false,                          // no boolean operation
+      0,                              // a copy number
+      checkOverlaps);                 // overlaps checking
+
+  //
+  // Front Endcap of Vacuum Vessel
+  //  - It works as a virtual detection volume to know
+  //  whether a particle is passing through this membrane
+  //  and so it incident to the vacuum vessel.
+  G4double l_sensor_volume = 1.0 * mm;
+  G4double r_sensor_volume = 10.0 * m;
+  G4Tubs* solid_sensor_volume =
+    new G4Tubs("sensor_volume",
+        0.,                       // inner radius
+        r_sensor_volume,                   // outer radius
+        0.5 * l_sensor_volume,    // half-length
+        0.,
+        2. * M_PI * rad );
+  G4LogicalVolume* logic_sensor_volume =
+    new G4LogicalVolume(solid_sensor_volume,
+        world_mat,
+        "sensor_volume");
+  new G4PVPlacement(0,        // no rotation
+      G4ThreeVector(0, 0, 0.5 * ( l_dump + 2*l_mod + l_sensor_volume ) ),        // where to put?
+      logic_sensor_volume,    // its logical volume
+      "sensor_volume",        // its name
+      logicWorld,             // its mother volume
+      false,                  // no boolean operation
+      0,                      // a copy number
+      checkOverlaps);         // overlaps checking
 
   //
   //always return the physical World
